b90ab2cd2abddbee8fb65e34102376ccb10cb33e
Antoine Buchser <antoine.buchser@epitech.eu>
2015-12-28 14:13:42 +0100

    fullscreen

--- CS - MyWindowsMediaPlayer/View/MainWindow.xaml.cs
+++ CS - MyWindowsMediaPlayer/View/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Drawing.Text;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,6 +15,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MyWindowsMediaPlayer.View
 {
@@ -22,9 +24,51 @@ namespace MyWindowsMediaPlayer.View
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool _fullscreen = false;
+        private DispatcherTimer _doubleClickTimer = new DispatcherTimer();
+        private GridLength[] _columnsWidth = new GridLength[3];
+
         public MainWindow()
         {
             InitializeComponent();
+            _doubleClickTimer.Interval = TimeSpan.FromMilliseconds(GetDoubleClickTime());
+            _doubleClickTimer.Tick += (s, e) => _doubleClickTimer.Stop();
         }
+
+        private void MediaPlayer_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (!_doubleClickTimer.IsEnabled)
+                _doubleClickTimer.Start();
+            else
+            {
+                if (!_fullscreen)
+                {
+                    this.WindowStyle = WindowStyle.None;
+                    this.WindowState = WindowState.Maximized;
+
+                    this.ContentSplitter.Visibility = Visibility.Collapsed;
+                    this.LeftContentFrame.Visibility = Visibility.Collapsed;
+                    _columnsWidth[0] = this.Content.ColumnDefinitions[0].Width;
+                    _columnsWidth[1] = this.Content.ColumnDefinitions[1].Width;
+                    this.Content.ColumnDefinitions[0].Width = new GridLength(0, GridUnitType.Auto);
+                    this.Content.ColumnDefinitions[1].Width = new GridLength(0, GridUnitType.Auto);
+                }
+                else
+                {
+                    this.WindowStyle = WindowStyle.SingleBorderWindow;
+                    this.WindowState = WindowState.Normal;
+
+                    this.Content.ColumnDefinitions[0].Width = _columnsWidth[0];
+                    this.Content.ColumnDefinitions[1].Width = _columnsWidth[1];
+                    this.ContentSplitter.Visibility = Visibility.Visible;
+                    this.LeftContentFrame.Visibility = Visibility.Visible;
+                }
+
+                _fullscreen = !_fullscreen;
+            }
+        }
+
+        [DllImport("user32.dll")]
+        private static extern uint GetDoubleClickTime();
     }
 }
