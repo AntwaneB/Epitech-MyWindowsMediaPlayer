219d70bbe219a38f0948c476d29a07ba90e077fb
brunne_s <steeven.brunner@epitech.eu>
2016-01-04 17:40:42 +0100

    Save playlist

--- CS - MyWindowsMediaPlayer/ViewModel/PlaylistListVM.cs
+++ CS - MyWindowsMediaPlayer/ViewModel/PlaylistListVM.cs
@@ -6,6 +6,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using System.Windows.Input;
 
 namespace MyWindowsMediaPlayer.ViewModel
@@ -67,6 +68,36 @@ namespace MyWindowsMediaPlayer.ViewModel
         }
         #endregion
 
+        #region Saving
+        public void SavePlaylist()
+        {
+
+            System.IO.StreamWriter file = new System.IO.StreamWriter("h:\\C_sharp_WMP\\epitech-mywindowsmediaplayer\\Save\\playlist.xml");
+
+            XElement save = new XElement("playlists");
+            String media = "";
+            String path = "";
+            foreach (var item in _playlists)
+            {
+                XElement playlistName = new XElement("playlist");
+                media = item.Name;
+                playlistName.SetAttributeValue("name", media);
+                foreach (var elem in item)
+                {
+                    XElement xml = new XElement("media");
+                    path = elem.Path.LocalPath;
+                    xml.SetAttributeValue("path", path);
+                    playlistName.Add(xml);
+                    System.Diagnostics.Debug.WriteLine(xml);
+                }
+                save.Add(playlistName);
+            }
+            System.Diagnostics.Debug.WriteLine(save);
+            file.WriteLine(save);
+            file.Close();
+        }
+        #endregion
+
         #region Ctor / Dtor
         public PlaylistListVM()
         {
@@ -74,7 +105,7 @@ namespace MyWindowsMediaPlayer.ViewModel
             _playlists.Add(new Playlist("Films"));
             _playlists.Add(new Playlist("Séries"));
             _playlists.Add(new Playlist("Photos de vacance"));
-            
+
             _playlists[0].Add(new Music(@"E:\Projets\CS - MyWindowsMediaPlayer\Example Medias\Music1.mp3"));
             _playlists[0].Add(new Music(@"E:\Projets\CS - MyWindowsMediaPlayer\Example Medias\MusicInfos1.mp3"));
 
@@ -82,6 +113,12 @@ namespace MyWindowsMediaPlayer.ViewModel
             _playlists[1].Add(new Video(@"E:\Projets\CS - MyWindowsMediaPlayer\Example Medias\Video1.mp4"));
 
             _playlists[2].Add(new Video(@"E:\Projets\CS - MyWindowsMediaPlayer\Example Medias\Video3.mp4"));
+
+            // Todo : A virer
+            //System.Diagnostics.Debug.WriteLine("**********");
+            SavePlaylist();
+            //System.Diagnostics.Debug.WriteLine("**********");
+            //
         }
         #endregion
     }
