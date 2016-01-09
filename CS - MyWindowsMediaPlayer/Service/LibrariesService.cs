9b545292cd33f63bf441dc5e93609a3133303f87
Antoine Buchser <antoine.buchser@epitech.eu>
2016-01-09 19:22:41 +0100

    librariesService

---
+++ CS - MyWindowsMediaPlayer/Service/LibrariesService.cs
@@ -0,0 +1,35 @@
+﻿using MyWindowsMediaPlayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWindowsMediaPlayer.Service
+{
+    class LibrariesService : List<Library<Media>>
+    {
+        #region Singleton
+        static LibrariesService _instance = new LibrariesService();
+
+        private LibrariesService() { }
+
+        static public LibrariesService Instance
+        {
+            get { return (_instance); }
+        }
+        #endregion
+
+        #region Methods
+        public void Import(string file)
+        {
+
+        }
+
+        public void Export(string file)
+        {
+            
+        }
+        #endregion
+    }
+}
